bc1a1ff6c266c3dbd67a464b278f2b7916eb7bbc
Jeshvenkatesh <Jeshvenkatesh@users.noreply.github.com>
2020-12-02 19:45:55 +0530

    initial commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const path = require('path');
-
+const hbs = require('hbs');
 const app = express();
 
 console.log(__dirname)
-console.log(__filename);
+// console.log(__filename);
 
 const publicDirectoryPath = path.join(__dirname,'../Public');
-console.log(publicDirectoryPath);
+//defining views path if we gave diff name like 'template' instead of 'views'
+const viewsPath = path.join(__dirname,'../template/views');
+const partialsPath = path.join(__dirname,'../template/partials');
 
+app.set('views',viewsPath)
 app.set('view engine','hbs'); //connecting hbs...
+hbs.registerPartials(partialsPath);   // registering partials...
+
 app.use(express.static(publicDirectoryPath));
 
 app.get('/',(req,res) => {       // Note: we use res.render() instead of res.send()
     res.render('index',{
         title:'Weather App',
-        name:"Venkatesh"
+        name:"Created By Venkatesh"
     });
 });
 app.get('/about',(req, res)=>{
     res.render('about',{
         title:'About Me',
-        name:'Venkatesh'
+        name:'Created by Venkatesh'
     });
 });
 app.get('/help',(req, res)=>{
     res.render('help',{
-        title:'help page',
-        name:'venkatesh'
+        title:'Help page',
+        name:' Created by venkatesh'
     });
 })
 app.get('/weather',(req, res) => {
@@ -37,10 +42,24 @@ app.get('/weather',(req, res) => {
     });
 });
 
+app.get('/help/*',(req,res) => {
+    // res.send('Help article not found')
+    res.render('helpError',{
+        message : "This is article is not found"
+    })
+})
+
+app.get('*',(req,res) => {
+    // res.send('<h4>page not found</h4>');
+    res.render('404',{
+        404 : 'Page not found'
+    })
+})
 app.listen(3000, () => {
     console.log('server is listening at 3000');
 });
 
+// Note: add extentions to it nodemon src/app.js -e js,hbs
 
 // app.get('/',(req, res) => {
 //     res.send('Hello express!')
