356faec4bf7056cc81450e1a2c851f8010c34ebe
Jeshvenkatesh <Jeshvenkatesh@users.noreply.github.com>
2020-12-03 12:08:15 +0530

    initial command
    Integrated the code, front end and backend

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,57 +2,88 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const app = express();
+const geoCode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 console.log(__dirname)
 // console.log(__filename);
 
-const publicDirectoryPath = path.join(__dirname,'../Public');
+const publicDirectoryPath = path.join(__dirname, '../Public');
 //defining views path if we gave diff name like 'template' instead of 'views'
-const viewsPath = path.join(__dirname,'../template/views');
-const partialsPath = path.join(__dirname,'../template/partials');
+const viewsPath = path.join(__dirname, '../template/views');
+const partialsPath = path.join(__dirname, '../template/partials');
 
-app.set('views',viewsPath)
-app.set('view engine','hbs'); //connecting hbs...
+app.set('views', viewsPath)
+app.set('view engine', 'hbs'); //connecting hbs...
 hbs.registerPartials(partialsPath);   // registering partials...
 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/',(req,res) => {       // Note: we use res.render() instead of res.send()
-    res.render('index',{
-        title:'Weather App',
-        name:"Created By Venkatesh"
+app.get('/', (req, res) => {       // Note: we use res.render() instead of res.send()
+    res.render('index', {
+        title: 'Weather App',
+        name: "Created By Venkatesh"
     });
 });
-app.get('/about',(req, res)=>{
-    res.render('about',{
-        title:'About Me',
-        name:'Created by Venkatesh'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Created by Venkatesh'
     });
 });
-app.get('/help',(req, res)=>{
-    res.render('help',{
-        title:'Help page',
-        name:' Created by venkatesh'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        name: ' Created by venkatesh'
     });
 })
-app.get('/weather',(req, res) => {
-    res.send({
-        forecast :'You will get weather forecast',
-        location : 'You will get location'
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide a address term'
+        })
+    }
+    geoCode(req.query.address, (error, { latitide, longititude, place_name }) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitide, longititude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecastdata: forecastData,
+                location: req.query.address
+            })
+        })
     });
-});
+    // res.send({
+    //     weather :'It is rainy outside',
 
-app.get('/help/*',(req,res) => {
-    // res.send('Help article not found')
-    res.render('helpError',{
-        message : "This is article is not found"
+    //     location : req.query.address
+    // });
+});
+app.get('/products', (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    res.send({
+        products: []
     })
 })
-
-app.get('*',(req,res) => {
+app.get('/help/*', (req, res) => {
+    // res.send('Help article not found')
+    res.render('helpError', {
+        message: "This is article is not found"
+    })
+})
+app.get('*', (req, res) => {
     // res.send('<h4>page not found</h4>');
-    res.render('404',{
-        404 : 'Page not found'
+    res.render('404', {
+        404: 'Page not found'
     })
 })
 app.listen(3000, () => {
